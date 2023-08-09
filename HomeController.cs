108aca66ef0b56eede74764f9d5b14cf9c53e1ff
Mkhuseli Ngetu <105724548+MkhuseliNgetu@users.noreply.github.com>
2023-08-09 22:22:37 +0100

    Initial Setup
    Initial Setup

---
+++ HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OuterHeaven
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(String SelectedExpierence)
+        {
+            if (SelectedExpierence.Equals("MaleExperience"))
+            {
+
+
+            }else if (SelectedExpierence.Equals("FemaleExperience"))
+            {
+
+            }
+            return View();
+        }
+    }
+}
