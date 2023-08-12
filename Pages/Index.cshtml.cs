162a2f616ee3bb7ee7eb566c35744edae13b14aa
Mkhuseli Ngetu <105724548+MkhuseliNgetu@users.noreply.github.com>
2023-08-12 18:11:38 +0100

    User Interface #1
    -Added Shopping Page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ namespace OuterHeaven.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+      
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,7 +14,38 @@ namespace OuterHeaven.Pages
 
         public void OnGet()
         {
+          
+        }
+        public IActionResult OnPost(String MaleExperienceButton, String FemaleExperienceButton)
+        {
+            if (FemaleExperienceButton == null)
+            {
 
+                if (MaleExperienceButton.Equals("Male"))
+                {
+                    ViewData["Experience"] = "Male";
+
+                }
+                else
+                {
+                    ViewData["Experience"] = "N/A";
+                }
+            }
+
+            if (MaleExperienceButton == null)
+            {
+                if (FemaleExperienceButton.Equals("Female") && FemaleExperienceButton != null)
+                {
+                    ViewData["Experience"] = "Female";
+                }
+                else
+                {
+                    ViewData["Experience"] = "N/A";
+                }
+
+            }
+
+            return RedirectToPage("Shop");
         }
     }
 }
---
+++ Pages/Shop.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OuterHeaven.Pages
+{
+    public class ShopModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+
+    }
+}
