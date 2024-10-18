a7074da944854da4ccdc57aef643c60a8d23009a
Mkhuseli Ngetu <105724548+MkhuseliNgetu@users.noreply.github.com>
2024-10-18 14:51:24 +0200

    Update #2.5
    -Added local images as fallback.
    
    -To Do:
    -Find out why azure blobs sometimes are indentified as missing.

--- app/user/login/page.tsx
+++ app/user/login/page.tsx
@@ -1,5 +1,8 @@
 import { Input } from "@nextui-org/input";
 import {Image} from "@nextui-org/image";
+import { Link } from "@nextui-org/link";
+import { Button } from "@nextui-org/button";
+import { CircleArrowRight, ShoppingBag } from "lucide-react";
 
 //Main Function
 export default function Main_Login(){
@@ -11,25 +14,29 @@ const ImagePaths: string[] = ["/Images/"]
         <section className="flex">
         {/* Image */}
         <div className="Login-Image-One">
-        <Image removeWrapper src={ImagePaths[0]+Images[0]} className="z-0 w-[40rem] h-[45rem] object-cover"/>
+        <Image removeWrapper src={ImagePaths[0]+Images[0]} className="z-0 w-[30rem] h-[45rem] object-cover"/>
         </div>
         {/* Image */}
         <div className="Login-Image-Two">
-        <Image removeWrapper src={ImagePaths[0]+Images[1]} className="z-0 w-[40rem] h-[45rem] object-cover"/>
+        <Image removeWrapper src={ImagePaths[0]+Images[1]} className="z-0 w-[30rem] h-[45rem] object-cover"/>
         </div>
         {/* Image */}
         <div className="Login-Image-Three">
-        <Image removeWrapper src={ImagePaths[0]+Images[2]} className="z-0 w-[40rem] h-[45rem] object-cover"/>
+        <Image removeWrapper src={ImagePaths[0]+Images[2]} className="z-0 w-[30rem] h-[45rem] object-cover"/>
         </div>
         {/* Form */}
         <div className="Login-Form">
 
-            <span className="title-font items-center">Login</span>
+            <span className="title-font">Login</span>
             <br />
-            <form action="" method="post" className="">
-            <Input color="default" isRequired type="email" label="Username" radius="lg"/>
+            <form action="" method="post">
+            <Input color="default" isRequired type="email" placeholder="Username" label="Username" radius="lg" size="md"/>
             <br />
-            <Input isRequired type="Password" label="Password" placeholder="Password" radius="lg"/>
+            <Input isRequired type="Password" label="Password" placeholder="Password" radius="lg" size="md"/>
+            <br/>
+            <Button href={'/main'} as={Link} size="lg" color="primary" radius="full" endContent={ <CircleArrowRight />}>
+                Login
+            </Button>
             </form>
         </div>
         </section>
